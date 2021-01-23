0a26dca2bb2cb9952e3402daf4c230fbed3badf5
GansaleZz <firemage1488@gmail.com>
2021-01-23 22:29:38 +0300

    Create Main.java

---
+++ FirstHW/HW/src/com/epam/jwd/app/Main.java
@@ -0,0 +1,10 @@
+package com.epam.jwd.app;
+
+import com.epam.jwd.model.Point;
+
+public class Main {
+    private static Point point = new Point();
+    public static void main(String[] args){
+        point.FirstAction();
+    }
+}
