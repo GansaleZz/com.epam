c84d609907f88486d0859db91adc67d32a6c9fb7
GansaleZz <firemage1488@gmail.com>
2021-01-28 19:57:23 +0300

    Update Square.java

--- SecHW/src/main/java/com/epam/jwd/model/Square.java
+++ SecHW/src/main/java/com/epam/jwd/model/Square.java
@@ -39,49 +39,46 @@ public class Square extends Figure{
 
 
     public void CheckExistance(){
-        boolean bool = false;
-            for(int i=1;i< masP.length;++i){
-                if(masP[0].getY() == masP[i].getY() && masP[0].getX() != masP[i].getX()){
-                    if(i == 1 ){
-                        if(masP[2].getX()+masP[3].getX() == masP[0].getX()+masP[1].getX() && masP[2].getY() == masP[3].getY()) {
-                            System.out.println(toString());
-                        }
-                        else{
-                            log.error("Square doesnot exist!");
-                            System.out.println("Square doesnt exist!");
-                        }
-                        bool = true;
-                        break;
-                    } else {
-                        if(i == 2 ) {
-                            if (masP[1].getX() + masP[3].getX() == masP[0].getX() + masP[2].getX() && masP[1].getY() == masP[3].getY()) {
-                                System.out.println(toString());
-                            } else {
-                                log.error("Square doesnt exist!");
-                                System.out.println("Square doesnt exist!");
-                            }
-                        }else{
-                            if (masP[1].getX() + masP[2].getX() == masP[0].getX() + masP[3].getX() && masP[1].getY() == masP[2].getY()) {
-                                System.out.println(toString());
-                            } else {
-                                log.error("Square doesnt exist!");
-                                System.out.println("Square doesnt exist!");
-                            }
-                        }
-                        bool = true;
-                        break;
+            double[] D = new double[6];
+
+            D[0] = masP[0].Distance(masP[1]);
+            D[1] = masP[0].Distance(masP[2]);
+            D[2] = masP[0].Distance(masP[3]);
+            D[3] = masP[1].Distance(masP[2]);
+            D[4] = masP[1].Distance(masP[3]);
+            D[5] = masP[2].Distance(masP[3]);
+
+            for(int i=0;i<D.length;++i){
+                double temp;
+                for(int j=0;j<D.length-1;++j){
+                    if(D[j]>D[j+1]){
+                        temp = D[j];
+                        D[j] = D[j+1];
+                        D[j+1] = temp;
                     }
                 }
             }
-            if (!bool){
-                log.error("Square doesnt exist!");
-                System.out.println("Square doesnt exist!");
+
+            for(int i = 5 ; i >-1; --i){
+                D[i] /= D[0];
             }
+
+            if((Math.abs(D[0]-1)<Math.pow(1,-8)&&
+                    Math.abs(D[1]-1)<Math.pow(1,-8)&&
+                    Math.abs(D[2]-1)<Math.pow(1,-8)&&
+                    Math.abs(D[3]-1)<Math.pow(1,-8)&&
+                    Math.abs(D[4]-Math.sqrt(2))<Math.pow(1,-8)&&
+                    Math.abs(D[5]-Math.sqrt(2))<Math.pow(1,-8)))
+            {System.out.println(toString());}else{
+                log.error("Square doesnt exist!");
+               System.out.println("Square doesnt exist!");
+            }
+
     }
 
     @Override
     public String toString(){
         log.info("Square exists! Points:  x1:("+masP[0].getX()+","+masP[0].getY()+"), x2:("+masP[1].getX()+","+masP[1].getY()+"), x3:("+masP[2].getX()+","+masP[2].getY()+"), x4:("+masP[3].getX()+","+masP[3].getY()+")");
-        return "Square exists! Points:  x1:("+masP[0].getX()+","+masP[0].getY()+"), x2:("+masP[1].getX()+","+masP[1].getY()+"), x3:("+masP[2].getX()+","+masP[2].getY()+")";
+        return "Square exists! Points:  x1:("+masP[0].getX()+","+masP[0].getY()+"), x2:("+masP[1].getX()+","+masP[1].getY()+"), x3:("+masP[2].getX()+","+masP[2].getY()+"), x4:("+masP[3].getX()+","+masP[3].getY()+")";
     }
 }
