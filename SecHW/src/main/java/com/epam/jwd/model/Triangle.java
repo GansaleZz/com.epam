0befab8633e59c9cdba3a3204214fb4764e6fe89
GansaleZz <firemage1488@gmail.com>
2021-01-28 23:55:17 +0300

    Update Triangle.java

--- SecHW/src/main/java/com/epam/jwd/model/Triangle.java
+++ SecHW/src/main/java/com/epam/jwd/model/Triangle.java
@@ -2,10 +2,13 @@ package com.epam.jwd.model;
 
 import java.lang.Math;
 import org.apache.log4j.Logger;
+import com.epam.jwd.model.*;
 
 public class Triangle extends Figure{
     private static final Logger log = Logger.getLogger(Triangle.class);
     private Point[] masP = new Point[3];
+    private Figure.FigureType Type = FigureType.TRIANGLE;
+    private double a,b,c;
 
     public Triangle(){
         for(int i=0;i<this.masP.length;++i){
@@ -36,18 +39,42 @@ public class Triangle extends Figure{
         return false;
     }
 
-    public void checkExistance(){
+    public boolean checkExistance(){
         double a,b,c;
         a = Math.pow(Math.pow(this.masP[0].getX() - this.masP[1].getX(),2)+Math.pow(this.masP[0].getY()-this.masP[1].getY(),2),0.5);
         b = Math.pow(Math.pow(this.masP[1].getX() - this.masP[2].getX(),2)+Math.pow(this.masP[1].getY()-this.masP[2].getY(),2),0.5);
         c = Math.pow(Math.pow(this.masP[0].getX() - this.masP[2].getX(),2)+Math.pow(this.masP[0].getY()-this.masP[2].getY(),2),0.5);
         if (a>=b+c || b>=a+c || c>=a+b){
-            log.error("Triangle doesnt exist!");
-            System.out.println("Triangle doesnt exist!");
+            return false;
         }else{
-            System.out.println(toString());
+            return true;
         }
 
+    }
+
+    public void existance(){
+        if(checkExistance()){
+            System.out.println(toString());
+        }else{
+            log.error("Triangle doesnt exist!");
+            System.out.println("Triangle doesnt exist!");
+        }
+    }
+
+    public Figure.FigureType getType(){
+        return this.Type;
+    }
+
+    public double getA(){
+        return this.a;
+    }
+
+    public double getB(){
+        return this.b;
+    }
+
+    public double getC(){
+        return this.c;
     }
 
     @Override
