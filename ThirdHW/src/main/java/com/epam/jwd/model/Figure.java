9d04fb5dc81eebe7f1f4ada84ee0cc12b8239410
GansaleZz <firemage1488@gmail.com>
2021-01-29 14:17:40 +0300

    Update Figure.java

--- ThirdHW/src/main/java/com/epam/jwd/model/Figure.java
+++ ThirdHW/src/main/java/com/epam/jwd/model/Figure.java
@@ -1,10 +1,14 @@
 package com.epam.jwd.model;
 
 
+import com.epam.jwd.strategy.Properties;
 import org.apache.log4j.Logger;
 
 public class Figure {
     private static int id = 0;
+    private FigureType type;
+    private Point[] masP;
+
 
     public void incrementID(){
         ++this.id;
@@ -20,11 +24,40 @@ public class Figure {
         SQUARE;
     }
 
+    public Figure(){
+    }
+
+    public Figure(FigureType type, Point[] masP){
+        this.type = type;
+        this.masP = masP;
+    }
+
+
+    public Point[] getMasP() {
+        return masP;
+    }
 
     public interface figurePropertiesStrategy{
          boolean oppToFindProperties(); //opportunity
          void Property();
     }
+
+//    public enum Propers{
+//        INSTANCE;
+//
+//
+//
+//        private FigureType type;
+//        private Point[] masP;
+//
+//        Propers(FigureType type, Point[] masP){
+//            this.type = type;
+//            this.masP = masP;
+//        }
+//
+//        private Properties figure = Properties.getInstance(this.type,this.masP);
+//
+//    }
 
     @Override
     public String toString(){
