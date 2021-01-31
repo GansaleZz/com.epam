5b5363b3413b7e4b3365452303c41ec12502b227
GansaleZz <firemage1488@gmail.com>
2021-02-01 00:02:18 +0300

    Update FigureExistencePostProcessor.java

--- ThirdHW/src/main/java/com/epam/jwd/impl/FigureExistencePostProcessor.java
+++ ThirdHW/src/main/java/com/epam/jwd/impl/FigureExistencePostProcessor.java
@@ -1,11 +1,20 @@
 package com.epam.jwd.impl;
 
-import com.epam.jwd.exception.FigureException;
+import com.epam.jwd.exception.FigureNotExistException;
+import com.epam.jwd.model.Figure;
+import com.epam.jwd.model.Line;
 import com.epam.jwd.service.FigurePostProcessor;
+import org.apache.log4j.Logger;
 
 public class FigureExistencePostProcessor implements FigurePostProcessor {
-    @Override
-    public void Figureprocess() throws FigureException {
+    private static final Logger log = Logger.getLogger(Line.class);
 
+    @Override
+    public void Figureprocess(boolean bool,Figure.FigureType type) throws FigureNotExistException {
+        if(bool){
+            log.info("Object "+type+" was created!");
+        }else{
+            log.error("Object "+type+" wasn't created");
+        }
     }
 }
