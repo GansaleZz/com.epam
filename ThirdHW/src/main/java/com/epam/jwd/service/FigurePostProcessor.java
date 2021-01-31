3322763a9b56f7abf6eab94deb7542a380d8bc92
GansaleZz <firemage1488@gmail.com>
2021-02-01 00:02:10 +0300

    Update FigurePostProcessor.java

--- ThirdHW/src/main/java/com/epam/jwd/service/FigurePostProcessor.java
+++ ThirdHW/src/main/java/com/epam/jwd/service/FigurePostProcessor.java
@@ -1,7 +1,8 @@
 package com.epam.jwd.service;
 
 import com.epam.jwd.exception.FigureException;
+import com.epam.jwd.model.Figure;
 
 public interface FigurePostProcessor {
-    void Figureprocess() throws FigureException;
+    void Figureprocess(boolean bool, Figure.FigureType type) throws FigureException;
 }
