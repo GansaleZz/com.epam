549ae838547578d7b5d258735d5e5ca07ae065fa
GansaleZz <firemage1488@gmail.com>
2021-01-31 15:27:13 +0300

    Update Main.java

--- ThirdHW/src/main/java/com/epam/jwd/Main.java
+++ ThirdHW/src/main/java/com/epam/jwd/Main.java
@@ -2,7 +2,6 @@ package com.epam.jwd;
 
 import com.epam.jwd.model.*;
 import com.epam.jwd.strategy.*;
-import org.apache.log4j.Logger;
 
 
 public class Main {
@@ -15,10 +14,14 @@ public class Main {
         InitL(masL);
         InitT(masT);
         InitS(masS);
+        MultiAngleFigure Pen = InitPen();
+        MultiAngleFigure Hex = InitHex();
         InfoAboutmasP(masP);
         InfoAboutmasL(masL);
         InfoAboutmasT(masT);
         InfoAboutmasS(masS);
+        InfoAboutPen(Pen);
+        InfoAboutHex(Hex);
     }
 
     public static void InitP(Point[] masP){
@@ -49,6 +52,18 @@ public class Main {
         }
     }
 
+    public static MultiAngleFigure InitPen(){
+        FigureFactory factory = new FigureFactory();
+        MultiAngleFigure Pen = (MultiAngleFigure) factory.getFigure(Figure.FigureType.PENTAGON);
+        return Pen;
+    }
+
+    public static MultiAngleFigure InitHex(){
+        FigureFactory factory = new FigureFactory();
+        MultiAngleFigure Hex = (MultiAngleFigure) factory.getFigure(Figure.FigureType.HEXAGON);
+        return Hex;
+    }
+
     public static void InfoAboutmasP(Point[] masP){
         int i = 0;
         do{
@@ -65,20 +80,38 @@ public class Main {
     }
 
     public static void InfoAboutmasT(Triangle[] masT){
-        for(int i=0;i<masT.length;++i){
-            if(!masT[i].checkSamePoints())
+        for(int i=0;i<2;++i){
+            if(!masT[i].checkSamePoints()) {
                 masT[i].existance();
-                Properties figure = Properties.getInstance(masT[i].getType(),masT[i].getTriangle());
-                figure.Property();
+                if(masT[i].checkExistance()) {
+                    Properties figure =  Properties.getInstance(masT[i].getType(), masT[i].getTriangle());
+                    figure.Property();
+                }
+            }
         }
     }
 
     public static void InfoAboutmasS(Square[] masS){
         for(int i=0;i<masS.length;++i){
-            if(!masS[i].checkSamePoints())
+            if(!masS[i].checkSamePoints()) {
                 masS[i].existance();
-                Properties figure = Properties.getInstance(masS[i].getType(),masS[i].getSquare());
-                figure.Property();
+                if(masS[i].checkExistance()) {
+                    Properties figure = Properties.getInstance(masS[i].getType(), masS[i].getSquare());
+                    figure.Property();
+                }
+            }
+        }
+    }
+
+    public static void InfoAboutPen(MultiAngleFigure Pen){
+        if(!Pen.checkSamePoints()){
+            Pen.existance();
+        }
+    }
+
+    public static void InfoAboutHex(MultiAngleFigure Hex){
+        if(!Hex.checkSamePoints()){
+            Hex.existance();
         }
     }
 }
