056e657d811b09aa963aba781e346f31d61f0c74
GansaleZz <firemage1488@gmail.com>
2021-01-31 23:00:53 +0300

    Create FigureExistencePostProcessor.java

---
+++ ThirdHW/src/main/java/com/epam/jwd/impl/FigureExistencePostProcessor.java
@@ -0,0 +1,11 @@
+package com.epam.jwd.impl;
+
+import com.epam.jwd.exception.FigureException;
+import com.epam.jwd.service.FigurePostProcessor;
+
+public class FigureExistencePostProcessor implements FigurePostProcessor {
+    @Override
+    public void Figureprocess() throws FigureException {
+
+    }
+}
