5a9b12afe3aee423c061a9ae74755a4792efd310
GansaleZz <firemage1488@gmail.com>
2021-02-06 22:59:23 +0300

    Delete FigurePostProcessor.java

--- ThirdHW/src/main/java/com/epam/jwd/service/FigurePostProcessor.java
+++
@@ -1,8 +0,0 @@
-package com.epam.jwd.service;
-
-import com.epam.jwd.exception.FigureException;
-import com.epam.jwd.model.Figure;
-
-public interface FigurePostProcessor {
-    void Figureprocess(boolean bool, Figure.FigureType type) throws FigureException;
-}
