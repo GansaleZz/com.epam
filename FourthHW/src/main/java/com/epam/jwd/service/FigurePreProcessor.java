0b86c1d21f8ba96f44c74c97ca1efa4294e43a58
GansaleZz <firemage1488@gmail.com>
2021-02-18 15:47:04 +0300

    Update

--- FourthHW/src/main/java/com/epam/jwd/service/FigurePreProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.epam.jwd.service;
-
-import com.epam.jwd.exception.FigureException;
-import com.epam.jwd.model.Figure;
-import com.epam.jwd.model.*;
-
-public interface FigurePreProcessor {
-    void figurePreProcess(Point[] masP, Figure.FigureType type) throws FigureException;
-}
