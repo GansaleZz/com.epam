5407014a9b06cb21ede26408442debd2efa10300
GansaleZz <firemage1488@gmail.com>
2021-02-18 15:40:32 +0300

    Deleted

--- FifthHW/src/main/java/com/epam/jwd/service/FigurePostProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.epam.jwd.service;
-
-import com.epam.jwd.exception.FigureException;
-import com.epam.jwd.model.Figure;
-import com.epam.jwd.model.FigureFactory;
-
-public interface FigurePostProcessor {
-    void figurepostprocess(boolean bool, Figure.FigureType type,Figure figure) throws FigureException;
-}
