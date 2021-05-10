256dc01356dfb13ac5e37ed3f4b9147cd18faa84
GansaleZz <72818524+GansaleZz@users.noreply.github.com>
2021-05-10 13:05:27 +0300

    Update ChangeUsersRS.java

--- FinalProject/src/main/java/com/epam/service/ChangeUsersRS.java
+++ FinalProject/src/main/java/com/epam/service/ChangeUsersRS.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+//Changing users 'Role' and 'Status'
 public class ChangeUsersRS implements Command{
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
